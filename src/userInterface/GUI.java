8e7c47fac511ae2c4178ddd28359ece9712b8ac4
Jarod Cahoon <jbc53@duke.edu>
2017-09-23 14:39:55 -0400

    Began to create the Grid of Cells within the panel

--- src/userInterface/GUI.java
+++ src/userInterface/GUI.java
@@ -10,24 +10,26 @@ import javafx.scene.layout.*;
 
 public class GUI extends Application{
 	public static final double BUTTON_MAX_WIDTH = 120;
+	public static final double GRID_SIZE = 300;
 	
 	private Stage mainStage;
 	private Scene mainScene;
 	private BorderPane Pane;
+	private GridPane cellGrid;
 	private TextField inputField;
+	
 	@Override
-	public void start(Stage pStage) throws Exception {
+	private void start(Stage pStage) throws Exception {
 		setMain(pStage);
 		
 	}
 
 	private void setMain(Stage pStage) {
 		mainStage = pStage;
-		mainStage.setTitle("Damn this is GUI");
-		Button mainButton = new Button("Please press me :D");
+		mainStage.setTitle("Cell Society Simulation");
+		setCellGrid();
 		setBorderPane();
-		Pane.getChildren().add(mainButton);
-		mainScene = new Scene(Pane, 500, 500);
+		mainScene = new Scene(Pane, 650, 650);
 		mainStage.setScene(mainScene);
 		mainStage.show();
 	}
@@ -43,22 +45,36 @@ public class GUI extends Application{
 		leftSideBox.setPadding(new Insets(10));
 		leftSideBox.setSpacing(13);
 		Pane.setLeft(leftSideBox);
+		
 		Button playButton = new Button("Play");
-		Button pauseButton = new Button("Pause");
-		Button stepButton = new Button("Step");
+		Button pauseButton = new Button("Pause (inactive)");
+		Button stepButton = new Button("Step (inactive)");
+		
 		playButton.setTranslateY(320);
 		pauseButton.setTranslateY(320);
 		stepButton.setTranslateY(320);
 		playButton.setMaxWidth(BUTTON_MAX_WIDTH);
 		pauseButton.setMaxWidth(BUTTON_MAX_WIDTH);
 		stepButton.setMaxWidth(BUTTON_MAX_WIDTH);
-		
 		leftSideBox.getChildren().addAll(playButton, pauseButton, stepButton);
 		
 		Label insLabel = new Label("Enter Name of XML File: ");
 		inputField = new TextField();
+		inputField.setPrefWidth(300);
+		inputField.setFocusTraversable(false);
+		inputField.setPromptText("For instance \"GOLSim.xml\"");
 		Pane.setTop(topBox);
+		Pane.setCenter(cellGrid);
+		Pane.setBottom(null);
+		Pane.setRight(null);
+		
 		topBox.getChildren().addAll(insLabel, inputField);
+	}
+	
+	private void setCellGrid() {
+		cellGrid = new GridPane();
+		cellGrid.setPrefHeight(GRID_SIZE);
+		cellGrid.setPrefWidth(GRID_SIZE);
 	}
 	
 	public static void main(String[] args) {
