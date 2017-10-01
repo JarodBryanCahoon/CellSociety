339a548621c00e9ef23d37ba57100e6a6a1e8928
Keegan O'Reilly <ko72@duke.edu>
2017-10-01 18:52:26 -0400

    for merge error

--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,20 @@ import userInterface.GUI;
 
 public class Main {
 	public static void main(String[] args){
-		// Currently for testing
+		// Currently for testing, creating xml Files
+		//for Fire simulation
 		String neighbors = "Hex";
 		String simType = "Fire";
 		String gridType = "Hexagon";
-		String defaultState = "1";
+		String defaultState = "0";
 		int numRows = 10;
 		int numColumns = 10;
 		int numStates = 3;
 		double[] parameters = new double[1];
 		parameters[0] = 0.8;
 		List<Integer> gridLocations = null;
-	
 		FileCreator.xmlCreator(neighbors, simType, gridType, defaultState, numRows, numColumns, numStates, parameters, gridLocations);
-		//testFireSimulation();
+	
 	}
 	
-	
-//	private static void testFireSimulation() throws Exception {
-//		Simulation sim = new FileHandler().fileReader("data\\Wator.xml");
-//		AbstractGrid cells = sim.getGrid();
-//		System.out.println(cells);
-//		System.out.println();
-//		sim.step();
-//		System.out.println(cells);
-//		System.out.println();
-//		sim.step();
-//		System.out.println(cells);
-//		System.out.println();
-//		sim.step();
-//		System.out.println(cells);
-//		System.out.println();
-//		sim.step();
-//	}
 }
